745c9aeff0f6434cc01f3d7d64de119f05f0c88f
olga <olgafff@free.fr>
2020-02-28 19:28:25 +0100

    feat:Data validations

--- src/Entity/Property.php
+++ src/Entity/Property.php
@@ -6,8 +6,12 @@ use DateTime;
 use Doctrine\ORM\Mapping as ORM;
 use Cocur\Slugify\Slugify;
 
+use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+
 /**
  * @ORM\Entity(repositoryClass="App\Repository\PropertyRepository")
+ * @UniqueEntity("title")
  */
 class Property
 {
@@ -24,6 +28,9 @@ class Property
     private $id;
 
     /**
+     * @Assert\Length(
+     *      min = 5,
+     *      max = 255)
      * @ORM\Column(type="string", length=255)
      */
     private $title;
@@ -34,7 +41,12 @@ class Property
     private $description;
 
     /**
+     * 
+     * 
      * @ORM\Column(type="integer")
+     * @Assert\Range(
+     *     min = 10,
+     *     max = 400)
      */
     private $surface;
 
@@ -75,6 +87,7 @@ class Property
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Assert\Regex("/^[0-9]{5}/")
      */
     private $postal_code;
 
